aac5f93f23985524a21a6c8b679552bbf5e6b42b
Cikal Amaro <143685377+MyLakic@users.noreply.github.com>
2025-09-22 23:08:54 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+// Smooth scroll sudah otomatis di CSS
+
+// Animasi Scroll Reveal
+function revealOnScroll() {
+  const reveals = document.querySelectorAll(".reveal");
+  for (let i = 0; i < reveals.length; i++) {
+    let windowHeight = window.innerHeight;
+    let elementTop = reveals[i].getBoundingClientRect().top;
+    let elementVisible = 100;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", revealOnScroll);
